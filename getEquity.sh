ca0636761a759a804be6eab8d0779503caf7a0ce
kam <kam@dhalik.me>
2015-01-18 11:08:45 -0500

    new things

---
+++ getEquity.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+curl -q -X POST https://54.174.49.59/request/blp/refdata/HistoricalData \
+ --cacert bloomberg.crt --cert mhacks_spring_2015_064.crt \
+ --key mhacks_spring_2015_064.key --data @- <<EOF
+{ "securities": ["$1"],
+  "fields": ["PX_LAST"],
+  "startDate": "$2",
+  "endDate": "$3",
+  "periodicitySelection": "DAILY",
+  "overrides": {
+    "px957": "1"
+  }
+}
+EOF
